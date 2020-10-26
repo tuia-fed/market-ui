ec439eddf99e38e05f96d7a670d02ad5f7be592e
zhujian <zhujian@tuia.cn>
2020-10-26 14:32:43 +0800

    init multiCubes

--- src/pages/multiCubes/index.tsx
+++ src/pages/multiCubes/index.tsx
@@ -65,22 +65,93 @@ export default defineComponent({
       <>
         <div class="nine-squared-wrap">
           <div class="top-squared-wrap">
-             <span class={"squared-item" + (state.activeIndex * 1 === 0 ? " active" :"")} style="background-color: yellow;"> 1</span>
-             <span class={"squared-item" + (state.activeIndex * 1 === 1 ? " active" :"")} style="background-color: green;"> 2 </span>
-             <span class={"squared-item" + (state.activeIndex * 1 === 2 ? " active" :"")} style="background-color: yellow;"> 3 </span>
+            <span
+              class={
+                'squared-item' + (state.activeIndex * 1 === 0 ? ' active' : '')
+              }
+              style="background-color: yellow;"
+            >
+              {' '}
+              1
+            </span>
+            <span
+              class={
+                'squared-item' + (state.activeIndex * 1 === 1 ? ' active' : '')
+              }
+              style="background-color: green;"
+            >
+              {' '}
+              2{' '}
+            </span>
+            <span
+              class={
+                'squared-item' + (state.activeIndex * 1 === 2 ? ' active' : '')
+              }
+              style="background-color: yellow;"
+            >
+              {' '}
+              3{' '}
+            </span>
           </div>
           <div class="right-squared-wrap">
-             <span class={"squared-item" + (state.activeIndex * 1 === 3 ? " active" :"")} style="background-color: green;">  4</span>
+            <span
+              class={
+                'squared-item' + (state.activeIndex * 1 === 3 ? ' active' : '')
+              }
+              style="background-color: green;"
+            >
+              {' '}
+              4
+            </span>
           </div>
           <div class="bottom-squared-wrap">
-             <span class={"squared-item" + (state.activeIndex * 1 === 4 ? " active" :"")} style="background-color: yellow;"> 5 </span>
-             <span class={"squared-item" + (state.activeIndex * 1 === 5 ? " active" :"")} style="background-color: green;"> 6 </span>
-             <span class={"squared-item" + (state.activeIndex * 1 === 6 ? " active" :"")} style="background-color: yellow;"> 7 </span>
+            <span
+              class={
+                'squared-item' + (state.activeIndex * 1 === 4 ? ' active' : '')
+              }
+              style="background-color: yellow;"
+            >
+              {' '}
+              5{' '}
+            </span>
+            <span
+              class={
+                'squared-item' + (state.activeIndex * 1 === 5 ? ' active' : '')
+              }
+              style="background-color: green;"
+            >
+              {' '}
+              6{' '}
+            </span>
+            <span
+              class={
+                'squared-item' + (state.activeIndex * 1 === 6 ? ' active' : '')
+              }
+              style="background-color: yellow;"
+            >
+              {' '}
+              7{' '}
+            </span>
           </div>
           <div class="left-squared-wrap">
-             <span class={"squared-item" + (state.activeIndex * 1 === 7 ? " active" :"")} style="background-color: green;"> 8 </span>
+            <span
+              class={
+                'squared-item' + (state.activeIndex * 1 === 7 ? ' active' : '')
+              }
+              style="background-color: green;"
+            >
+              {' '}
+              8{' '}
+            </span>
           </div>
-          <div class="button" onClick={() => {start(7)}}>抽奖</div>
+          <div
+            class="button"
+            onClick={() => {
+              start(7)
+            }}
+          >
+            抽奖
+          </div>
           <span class="active-circle"></span>
         </div>
         <button onClick={autoActive}>重置</button>
