9392e042bd0b3302a5d6162a471f9882b319101b
zhujian <453424829@qq.com>
2020-11-20 09:50:29 +0800

    fix(.huskyrc.js): 恢复pre-commit

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
-    // 'pre-commit': 'npm run precommit',
-    // 'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
+    'pre-commit': 'npm run precommit',
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
   }
 }
