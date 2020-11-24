b52325f37fc67b1361b2293beb3d12caccdc6c5d
minjs.cn <zm@tuia.cn>
2020-11-24 14:53:17 +0800

    docs(docs): logo and responsive

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,16 @@
 import { defineComponent } from 'vue'
 import styles from './index.module.less'
 
+import logo from '@/assets/logo-revert.png'
+import logoText from '@/assets/logo-text-revert.png'
+
 export default defineComponent({
   setup() {
     return () => (
       <header class={styles.header}>
         <div class={styles.left}>
-          <img
-            class={styles.brand}
-            src="https://yun.tuia.cn/tuia/tuia-advert-home-node/dist/8002c0da0e204d0717b18dc2136a87c4.png"
-            alt="LOGO"
-          />
+          <img class={styles.brand} src={logo} alt="LOGO" />
+          <img class={styles.text} src={logoText} alt="LOGO_TEXT" />
         </div>
         <div class={styles.right}>
           <a
