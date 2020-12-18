5ba93ac888f37436d7fdb19ece599521835eff05
lijingyong <lijingyong@duiba.com.cn>
2020-12-18 18:57:42 +0800

    feat: 布局样式修改

--- packages/turnCard/src/main.tsx
+++ packages/turnCard/src/main.tsx
@@ -49,37 +49,14 @@ export default createComponent({
   setup(props) {
     // 给组件生成一个唯一ID
     const id = ref('turncard-' + ++uid)
-    // 计算组件容器的宽度
-    const autoSize = ref(0)
-
-    onMounted(() => {
-      // 组件挂载成功之后，才能拿到真实的组件容器宽度
-      const el = document.getElementById(id.value)
-      const rect = (el?.parentNode as Element).getBoundingClientRect()
-      autoSize.value = rect.width
-    })
-
-    // 计算列间距的总和
-    const gaps = computed(() => {
-      return (props.column - 1) * props.columnGap
-    })
-
-    // 根据容器宽度，列间距，卡片宽高比，计算卡片的真实宽高
-    const rect = computed(() => {
-      const width = (autoSize.value - gaps.value) / props.column
-      return {
-        width,
-        height: (width * props.height) / props.width
-      }
-    })
 
     return () => (
       <div class={styles.container} id={id.value}>
         {props.options.map((item, i) => (
           <Card
             onClick={props.onCardClick}
-            width={rect.value.width}
-            height={rect.value.height}
+            width={props.width}
+            height={props.height}
             index={i}
             option={item}
             activeName={i === props.activeIndex ? props.activeClassName : ''}
