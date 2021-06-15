f3db43fcbafcc29c795e29bfe53d2889e55c478c
maobao <maobao@duiba.com.cn>
2021-06-15 16:12:17 +0800

    fix: 修改toast组件挂载全局的插件名称

--- src/toast/demo/index.vue
+++ src/toast/demo/index.vue
@@ -1,13 +1,13 @@
 <template>
   <demo-section>
-    <demo-block card title="基础用法">
+    <demo-block card title="基础用法1">
       <!-- #region html1 -->
       <button class="toast-button primary" @click="open">
         <span>Toast提示</span>
       </button>
       <!-- #endregion html1 -->
     </demo-block>
-    <demo-block card title="基础用法">
+    <demo-block card title="基础用法2">
       <!-- #region html2 -->
       <button class="toast-button primary" @click="openToast">
         <span>Toast提示</span>
@@ -21,10 +21,10 @@
 export default {
   methods: {
     open() {
-      this.$toast.show("这是一个toast提示");
+      this.$mkToast.show("这是一个toast提示");
     },
     openToast() {
-      this.$toast.show({
+      this.$mkToast.show({
         msg: "这是一个顶部的toast提示",
         position: "top",
         duration: 2500,
--- src/toast/index.ts
+++ src/toast/index.ts
@@ -1,26 +1,28 @@
-import { VueConstructor } from "vue";
+import Vue, { VueConstructor } from "vue";
 import Toast from "./src/index";
 
 type toastType = {
-  position?: string
-  showToast?: boolean
-  msg: string
-  color?: string
-  bg?: string
-  duration?: number
-}
+  position?: string;
+  showToast?: boolean;
+  msg: string;
+  color?: string;
+  bg?: string;
+  duration?: number;
+};
+
+const ToastConstructor = Vue.extend(Toast);
 
 (Toast as any).install = function (Vue: VueConstructor) {
   // 如果toast还在，则不再执行
   if (document.getElementsByClassName("toast_pane").length) {
     return;
   }
-  const toastTpl = Vue.extend(Toast);
-  const $vm: any = new toastTpl(); // 实例化vue实例
+  // const toastTpl = Vue.extend(Toast);
+  const $vm: any = new ToastConstructor(); // 实例化vue实例
   const tpl = $vm.$mount().$el;
   document.body.appendChild(tpl);
 
-  Vue.prototype.$toast = {
+  Vue.prototype.$mkToast = {
     show: function (options: string | toastType) {
       if (document.getElementsByClassName("toast_pane").length) {
         return;
@@ -35,24 +37,27 @@ type toastType = {
         setTimeout(() => {
           $vm.showToast = false;
         }, 2500);
-      } else if (typeof options === 'object') {
-        Object.assign($vm, options)
-        $vm.showToast = true
-        if(!options.bg && options.position && options.position !== 'top') {
-          $vm.bg = 'rgba(0, 0, 0, .7)'
-        }else {
-          $vm.bg = 'rgba(250, 100, 100, .9)'
+      } else if (typeof options === "object") {
+        Object.assign($vm, options);
+        $vm.showToast = true;
+        if (!options.bg && options.position && options.position !== "top") {
+          $vm.bg = "rgba(0, 0, 0, .7)";
+        } else {
+          $vm.bg = "rgba(250, 100, 100, .9)";
         }
-        if (!!options.duration) {
+        if (options.duration) {
           setTimeout(() => {
-            $vm.showToast = false
-          }, options.duration)
+            $vm.showToast = false;
+          }, options.duration);
         } else {
           setTimeout(() => {
-            $vm.showToast = false
-          }, 2500)
+            $vm.showToast = false;
+          }, 2500);
         }
       }
+    },
+    hide: function () {
+      $vm.showToast = false;
     },
   };
 };
