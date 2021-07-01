54756bb35809945d8240843a97f29456b137e242
maobao <maobao@duiba.com.cn>
2021-07-01 18:02:34 +0800

    feat: 关闭eslint semi代码末尾分号的校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "semi": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off"
   },
 };
