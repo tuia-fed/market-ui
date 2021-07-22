bb20e7fcb22c7217f53918304987bc9dd0f3ebda
maobao <maobao@duiba.com.cn>
2021-07-22 23:02:19 +0800

    style: 代码格式化yeah

--- src/TurnCard/src/index.tsx
+++ src/TurnCard/src/index.tsx
@@ -8,7 +8,7 @@ export default Vue.extend({
     // 组件父容器的尺寸
     container: {
       width: 0,
-      height: 0
+      height: 0,
     },
   }),
 
@@ -17,85 +17,98 @@ export default Vue.extend({
     cards: {
       type: Array as PropType<Array<CardImg>>,
       default: () => [],
-      required: true
+      required: true,
     },
     // 卡牌的翻转状态
     cardstate: {
       type: Array as PropType<Array<number>>,
       default: () => [],
-      validator: function(val) { // 状态校验-卡牌的状态参数必须为1、2、3、4四种类型其中一种
-        return val.every(item => [1, 2, 3, 4].indexOf(item) !== -1)
-      }
+      validator: function (val) {
+        // 状态校验-卡牌的状态参数必须为1、2、3、4四种类型其中一种
+        return val.every((item) => [1, 2, 3, 4].indexOf(item) !== -1);
+      },
     },
     // 行卡牌数目
     rowsAmount: {
       type: Number,
-      default: 3
-    }
+      default: 3,
+    },
   },
 
   methods: {
     /** 计算卡牌的背景 */
     configbackground(state: number, card: CardImg): string {
-      return state > 2 ? `url(${card.back})` : `url(${card.front})`
+      return state > 2 ? `url(${card.back})` : `url(${card.front})`;
     },
     cardNativeClick(index: number) {
-      if (this.cardstate[index] > 1) return false
-      this.$emit('card-start', index)
+      if (this.cardstate[index] > 1) return false;
+      this.$emit("card-start", index);
     },
     /** 获取父元素的尺寸 */
     getParentRect() {
-      const Turncard = document.getElementById('mkTurncard') as HTMLDivElement
-      const cardParent = Turncard.parentElement
-      const parentRect = cardParent?.getBoundingClientRect()
+      const Turncard = document.getElementById("mkTurncard") as HTMLDivElement;
+      const cardParent = Turncard.parentElement;
+      const parentRect = cardParent?.getBoundingClientRect();
       return {
         width: parentRect?.width || 320,
-        height: parentRect?.height || 320
-      }
+        height: parentRect?.height || 320,
+      };
     },
     /** 计算卡牌的尺寸 */
     calcardsize(size: CardSize) {
-      const cardW = Math.floor(size.width / this.rowsAmount) // 卡牌宽
-      const result = this.cards.length / this.rowsAmount
-      const columns = Number.isInteger(result) ? result : Math.round(result) // 牌堆的列数
-      const cardH = Math.floor(size.height / columns) // 卡牌高
-      const cardArr = Array.from({ length: this.cards.length })
+      const cardW = Math.floor(size.width / this.rowsAmount); // 卡牌宽
+      const result = this.cards.length / this.rowsAmount;
+      const columns = Number.isInteger(result) ? result : Math.round(result); // 牌堆的列数
+      const cardH = Math.floor(size.height / columns); // 卡牌高
+      const cardArr = Array.from({ length: this.cards.length });
       for (let i = 0; i < cardArr.length; i++) {
-        const cardDom = document.getElementById(`card${i}`) as HTMLDivElement
-        cardDom.style.width = `${cardW}px`
-        cardDom.style.height = `${cardH}px`
+        const cardDom = document.getElementById(`card${i}`) as HTMLDivElement;
+        cardDom.style.width = `${cardW}px`;
+        cardDom.style.height = `${cardH}px`;
       }
-    }
+    },
   },
 
   mounted() {
     this.$nextTick(() => {
-      const { width, height } = this.getParentRect()
+      const { width, height } = this.getParentRect();
 
-      this.container.width = width
-      this.container.height = height
-      this.calcardsize(this.container)
-    })
+      this.container.width = width;
+      this.container.height = height;
+      this.calcardsize(this.container);
+    });
   },
 
   render() {
     return (
       <div class="mk-turncard-main" id="mkTurncard">
-        {
-          this.cards.length && this.cards.map((card, index) => {
+        {this.cards.length &&
+          this.cards.map((card, index) => {
             return (
               <div class="mk-turncard-card__box" key={index}>
                 <div
                   id={`card${index}`}
-                  class={['mk-turncard-card__item', { 'mk-turncard-card__item-turn': this.cardstate[index] > 1 }, { 'mk-turncard-card__item-turn2': this.cardstate[index] > 2 }]}
-                  style={{ backgroundImage: this.configbackground(this.cardstate[index], card) }}
+                  class={[
+                    "mk-turncard-card__item",
+                    {
+                      "mk-turncard-card__item-turn": this.cardstate[index] > 1,
+                    },
+                    {
+                      "mk-turncard-card__item-turn2": this.cardstate[index] > 2,
+                    },
+                  ]}
+                  style={{
+                    backgroundImage: this.configbackground(
+                      this.cardstate[index],
+                      card
+                    ),
+                  }}
                   onClick={() => this.cardNativeClick(index)}
                 ></div>
               </div>
-            )
-          })
-        }
+            );
+          })}
       </div>
-    )
+    );
   },
 });
